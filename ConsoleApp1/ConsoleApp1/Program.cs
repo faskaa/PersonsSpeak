1665daa64d960b495cd6300adef54b1e16b34c84
faskaa <fazilnb@code.edu.az>
2023-08-22 14:21:22 +0400

    First Commit Initizailed

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace ConsoleApp1
+{
+    public interface IPerson
+    {
+        public void Speak();
+                      
+    }
+    
+    public class RussianPerson : IPerson
+    {
+        public void Speak()
+        {
+            Console.WriteLine("Здравствуйте");
+        }
+    }
+    public class FrenchPerson : IPerson 
+    {
+        public void Speak()
+        {
+            Console.WriteLine("Bonjour");
+        }
+    }
+    public class SpanishPerson : IPerson
+    {
+        public void Speak()
+        {
+            Console.WriteLine("Hola");
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
